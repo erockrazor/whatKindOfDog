679d5280539e17cdd98981be3423d686e5aa0c9b
Eric P. Gieg <erockusd@Hotmail.com>
2017-06-08 14:13:16 -0400

    added jquery

--- javascript.js
+++ javascript.js
@@ -1,18 +1,44 @@
-(function () {
+$(document).ready(function() {
+  $("#afterSubmission").hide();
+
+  $("#pictureButton").click(function() {
+    $("#submission").hide();
+    video.pause();
+    $("#afterSubmission").show();
+
+  });
+  $("#pictureButtonReset").click(function() {
+    $("#afterSubmission").hide();
+    $("#submission").show();
+    video.play();
+
+    // $("#submission").replaceWith('  "  <div id="submission">' +
+    //   '<button type="button" id="pictureButton" class="btn btn-success center-block"><i class="fa fa-5x fa-camera" aria-hidden="true"></i></button></div>"');
+  });
+
+
+  (function() {
     navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
 
     navigator.getMedia(
-        // constraints
-        {video:true, audio:false},
+      // constraints
+      {
+        video: true,
+        audio: false
+      },
 
-        // success callback
-        function (mediaStream) {
-            var video = document.getElementsByTagName('video')[0];
-            video.src = window.URL.createObjectURL(mediaStream);
-            video.play();
-        },
-        //handle error
-        function (error) {
-            console.log(error);
-        })
-})();
+      // success callback
+      function(mediaStream) {
+        var video = document.getElementsByTagName('video')[0];
+        video.src = window.URL.createObjectURL(mediaStream);
+        video.play();
+      },
+      //handle error
+      function(error) {
+        console.log(error);
+      });
+  })();
+
+
+
+});
