f0b830afae299412176b26532b675cb52e70875e
Eric P. Gieg <erockusd@Hotmail.com>
2017-06-08 22:17:38 -0400

    testing webcam

--- javascript.js
+++ javascript.js
@@ -3,14 +3,12 @@ $(document).ready(function() {
 
   $("#pictureButton").click(function() {
     $("#submission").hide();
-    video.pause();
     $("#afterSubmission").show();
 
   });
   $("#pictureButtonReset").click(function() {
     $("#afterSubmission").hide();
     $("#submission").show();
-    video.play();
 
   });
 
@@ -34,27 +32,7 @@ $(document).ready(function() {
   });
 
   sayCheese.start();
-  // (function() {
-  //   navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
-  //
-  //   navigator.getMedia(
-  //     // constraints
-  //     {
-  //       video: true,
-  //       audio: false
-  //     },
-  //
-  //     // success callback
-  //     function(mediaStream) {
-  //       var video = document.getElementsByTagName('video')[0];
-  //       video.src = window.URL.createObjectURL(mediaStream);
-  //       video.play();
-  //     },
-  //     //handle error
-  //     function(error) {
-  //       console.log(error);
-  //     });
-  // })();
+
 
 
 
