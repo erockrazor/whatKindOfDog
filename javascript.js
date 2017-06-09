1b40fd26d5512789ac7d1e4683ebda310c1a4fd9
Eric P. Gieg <erockusd@Hotmail.com>
2017-06-08 22:07:42 -0400

    added camera library

--- javascript.js
+++ javascript.js
@@ -1,37 +1,60 @@
 $(document).ready(function() {
   $("#afterSubmission").hide();
 
-  (function() {
-    navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
+  $("#pictureButton").click(function() {
+    $("#submission").hide();
+    video.pause();
+    $("#afterSubmission").show();
 
-    navigator.getMedia(
-      // constraints
-      {
-        video: true,
-        audio: false
-      },
-
-      // success callback
-      function(mediaStream) {
-        var video = document.getElementsByTagName('video')[0];
-        video.src = window.URL.createObjectURL(mediaStream);
-        video.play();
-      },
-      //handle error
-      function(error) {
-        console.log(error);
-
-        $("#pictureButton").click(function() {
-          $("#submission").hide();
-          video.pause();
-          $("#afterSubmission").show();
-
-        });
-        $("#pictureButtonReset").click(function() {
-          $("#afterSubmission").hide();
-          $("#submission").show();
-          video.play();
-        });
-      })();
   });
+  $("#pictureButtonReset").click(function() {
+    $("#afterSubmission").hide();
+    $("#submission").show();
+    video.play();
+
+  });
+
+  var sayCheese = new SayCheese('#videoRow', {
+    snapshots: true
+  });
+
+  sayCheese.on('start', function() {
+    // do something when started
+    this.takeSnapshot();
+  });
+
+  sayCheese.on('error', function(error) {
+    // handle errors, such as when a user denies the request to use the webcam,
+    // or when the getUserMedia API isn't supported
+  });
+
+  sayCheese.on('snapshot', function(snapshot) {
+    // do something with a snapshot canvas element, when taken
+  });
+
+  sayCheese.start();
+  // (function() {
+  //   navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
+  //
+  //   navigator.getMedia(
+  //     // constraints
+  //     {
+  //       video: true,
+  //       audio: false
+  //     },
+  //
+  //     // success callback
+  //     function(mediaStream) {
+  //       var video = document.getElementsByTagName('video')[0];
+  //       video.src = window.URL.createObjectURL(mediaStream);
+  //       video.play();
+  //     },
+  //     //handle error
+  //     function(error) {
+  //       console.log(error);
+  //     });
+  // })();
+
+
+
 });
